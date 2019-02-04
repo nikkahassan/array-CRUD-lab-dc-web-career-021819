3cddd774bd49eac9d40a8c81d3a9773e60c68f75
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-04 19:18:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
   end
 
 def add_element_to_end_of_array(array, element)
-  return friends.push("Taelor")
+  return friends.push(element)
   end
 
 def add_element_to_start_of_array(array, element)
-  return friends.unshift("Sara")
+  return friends.unshift(element)
   end
 
 def remove_element_from_end_of_array(array)
-  return friends.pop
+  return friends.pop()
   end
 
 def remove_element_from_start_of_array(array)
-  return friends.shift
+  friends.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  return friends[2]
+  friends[2]
 end
 
 def retrieve_first_element_from_array(array)
-  return friends[0]
+  friends[0]
 end
 
 def retrieve_last_element_from_array(array)
-  return friends[-1]
+  friends[-1]
 end
