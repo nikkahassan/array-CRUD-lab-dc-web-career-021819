f29d956d695527a6f987c751660a7963d99efb7c
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-04 19:11:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,7 @@ def add_element_to_end_of_array(array, element)
 
 def add_element_to_start_of_array(array, element)
   friends.unshift("Sara")
-  
-end
+  end
 
 def remove_element_from_end_of_array(array)
   
